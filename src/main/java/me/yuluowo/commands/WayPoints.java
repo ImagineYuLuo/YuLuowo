2c05fb30b9a3d31b42c3fc23ec9a3c39a029b136
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-02 17:29:57 +0800

    Create WayPoints.java

---
+++ src/main/java/me/yuluowo/commands/WayPoints.java
@@ -0,0 +1,67 @@
+package me.yuluowo.commands;
+
+import me.yuluowo.utils.ChatUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumChatFormatting;
+
+public class WayPoints {
+
+    static int playerX, playerY, playerZ;
+
+    public static void showWayPoints(Boolean Enable){
+        
+    }
+
+    public static class getWayPoints extends CommandBase {
+
+        @Override
+        public String getCommandName() {
+            return "waypoint";
+        }
+
+        @Override
+        public String getCommandUsage(ICommandSender sender) {
+            return "/" + getCommandName();
+        }
+
+        @Override
+        public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+            if(args.length == 0){
+                //set waypoint here (x, y, z)
+                BlockPos blockPos = Minecraft.getMinecraft().thePlayer.getPosition();
+                playerX = blockPos.getX();
+                playerY = blockPos.getY();
+                playerZ = blockPos.getZ();
+
+            }else switch (args[0]){
+                case "help":
+                    //help
+                    break;
+                case "on":
+                case "enable":
+                    showWayPoints(true);
+                    break;
+                case "off":
+                case "disable":
+                    showWayPoints(false);
+                    break;
+                default:
+                    ChatUtils.send(EnumChatFormatting.RED + "Unknown Command ...");
+            }
+        }
+
+        @Override
+        public int getRequiredPermissionLevel(){
+            return 0;
+        }
+    }
+
+
+
+
+
+}
