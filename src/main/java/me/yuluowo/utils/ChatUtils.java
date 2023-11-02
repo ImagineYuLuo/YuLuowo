c97edf395a529d52b384a54efb55757df0790e4d
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-02 17:29:59 +0800

    Update ChatUtils.java

--- src/main/java/me/yuluowo/utils/ChatUtils.java
+++ src/main/java/me/yuluowo/utils/ChatUtils.java
@@ -2,12 +2,13 @@ package me.yuluowo.utils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 
 public class ChatUtils {
 
-    public static void send(String content){
+    public static void send(String content) {
         Minecraft mc = Minecraft.getMinecraft();
-        if(mc.thePlayer != null){
+        if (mc.thePlayer != null) {
             mc.thePlayer.addChatComponentMessage(new ChatComponentText(content));
         }
     }
