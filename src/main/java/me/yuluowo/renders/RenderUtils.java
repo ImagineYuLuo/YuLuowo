22a7fdf6c9ffcf92fbcff9fa77d123e67d11dc60
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-03 18:01:15 +0800

    Create RenderUtils.java

---
+++ src/main/java/me/yuluowo/renders/RenderUtils.java
@@ -0,0 +1,27 @@
+package me.yuluowo.renders;
+
+import me.yuluowo.commands.WayPoints;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+public class RenderUtils {
+
+    @SubscribeEvent
+    public void onRenderTick(TickEvent.RenderTickEvent event){
+        int x = WayPoints.playerX;
+        int y = WayPoints.playerY;
+        int z = WayPoints.playerZ;
+        Minecraft mc = Minecraft.getMinecraft();
+        FontRenderer fontRenderer = mc.fontRendererObj;
+        if(mc.theWorld != null){
+            if(WayPoints.showWayPoint()){
+                fontRenderer.drawString("Waypoint set at : " + x + " " + y + " " + z, 5, 5, 0);
+            }else {
+                fontRenderer.drawString(null, 5, 5, 0);
+            }
+        }
+    }
+
+}
