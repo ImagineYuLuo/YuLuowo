502e86a40db534dccfcc4962e13ad7736af555fc
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-08 11:16:32 +0800

    Update Utils.java

--- src/main/java/me/yuluowo/utils/Utils.java
+++ src/main/java/me/yuluowo/utils/Utils.java
@@ -1,14 +1,50 @@
 package me.yuluowo.utils;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.EnumChatFormatting;
 
 public class Utils {
 
     public static boolean isOnHypixel(){
         Minecraft mc = Minecraft.getMinecraft();
+        //mc.thePlayer.getHeldItem().getDisplayName()
         if(mc != null && mc.theWorld != null && !mc.isSingleplayer()){
             return mc.getCurrentServerData().serverIP.toLowerCase().contains("hypixel");
         }
         return false;
     }
+
+    public static String[] rarityArrays = new String[]{
+        "COMMON",
+        "UNCOMMON",
+        "RARE",
+        "EPIC",
+        "LEGENDARY",
+        "MYTHIC",
+        "DIVINE",
+        "SPECIAL",
+        "VERY SPECIAL"
+    };
+
+    public static String[] rarityArraysColor = new String[]{
+        EnumChatFormatting.WHITE + EnumChatFormatting.BOLD.toString() + "COMMON",
+        EnumChatFormatting.GREEN + EnumChatFormatting.BOLD.toString() + "UNCOMMON",
+        EnumChatFormatting.BLUE + EnumChatFormatting.BOLD.toString() + "RARE",
+        EnumChatFormatting.DARK_PURPLE + EnumChatFormatting.BOLD.toString() + "EPIC",
+        EnumChatFormatting.GOLD + EnumChatFormatting.BOLD.toString() + "LEGENDARY",
+        EnumChatFormatting.LIGHT_PURPLE + EnumChatFormatting.BOLD.toString() + "MYTHIC",
+        EnumChatFormatting.AQUA + EnumChatFormatting.BOLD.toString() + "DIVINE",
+        EnumChatFormatting.RED + EnumChatFormatting.BOLD.toString() + "SPECIAL",
+        EnumChatFormatting.RED + EnumChatFormatting.BOLD.toString() + "VERY SPECIAL"
+    };
+
+    public static String heldItem(){
+        return Minecraft.getMinecraft().thePlayer.getHeldItem().getDisplayName();
+    }
+
+
+
+
+
+
 }
