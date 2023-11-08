90ca4086b3d69dc0d01750ae33c6644cd55ea693
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-08 11:16:22 +0800

    Create DisplayItemName.java

---
+++ src/main/java/me/yuluowo/commands/DisplayItemName.java
@@ -0,0 +1,39 @@
+package me.yuluowo.commands;
+
+import me.yuluowo.utils.ChatUtils;
+import me.yuluowo.utils.Utils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.EnumChatFormatting;
+
+public class DisplayItemName extends CommandBase {
+
+    @Override
+    public String getCommandName() {
+        return "display";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return "/" + getCommandName();
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        Minecraft mc = Minecraft.getMinecraft();
+        ChatUtils.send(EnumChatFormatting.GOLD + "You're holding" +
+                EnumChatFormatting.LIGHT_PURPLE + "[" +
+                EnumChatFormatting.BLACK + Utils.heldItem() +
+                EnumChatFormatting.LIGHT_PURPLE + "]" +
+                EnumChatFormatting.GOLD + "right now.");
+    }
+
+    //Giving Permission To Use Commands
+    @Override
+    public int getRequiredPermissionLevel(){
+        return 0;
+    }
+
+}
