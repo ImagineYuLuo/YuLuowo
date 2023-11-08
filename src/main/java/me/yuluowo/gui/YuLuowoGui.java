5b4af7f495e134e02ae6aeee749d6c49376457d4
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-08 11:16:26 +0800

    Create YuLuowoGui.java

---
+++ src/main/java/me/yuluowo/gui/YuLuowoGui.java
@@ -0,0 +1,15 @@
+package me.yuluowo.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class YuLuowoGui extends GuiScreen {
+
+    public void initGui(){
+
+    }
+
+    public void drawScreen(int mouseX, int mouseY, float partialTicks){
+
+    }
+
+}
