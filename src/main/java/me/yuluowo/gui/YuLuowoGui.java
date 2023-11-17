5e0f8efb7b7c410c4fc6d974ee8e8750a1f278ea
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-17 17:09:17 +0800

    Update YuLuowoGui.java

--- src/main/java/me/yuluowo/gui/YuLuowoGui.java
+++ src/main/java/me/yuluowo/gui/YuLuowoGui.java
@@ -1,15 +1,45 @@
 package me.yuluowo.gui;
 
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class YuLuowoGui extends GuiScreen {
 
-    public void initGui(){
+    private GuiButton btn_Close;
 
+    @Override
+    public void initGui() {
+        super.initGui();
+        // drawRect mean background
+        this.buttonList.add(btn_Close = new GuiButton(0, 300, 300, "Close"));
     }
 
-    public void drawScreen(int mouseX, int mouseY, float partialTicks){
-
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        if(button == btn_Close){
+            mc.thePlayer.closeScreen();
+        }
     }
 
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+    @Override
+    public boolean doesGuiPauseGame() {
+        return true;
+    }
 }
