2bf51567f059ab283ec88e1d760b6d74e5385212
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-20 20:17:04 +0800

    Update YuLuowoGui.java

--- src/main/java/me/yuluowo/gui/YuLuowoGui.java
+++ src/main/java/me/yuluowo/gui/YuLuowoGui.java
@@ -1,11 +1,12 @@
 package me.yuluowo.gui;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL13;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.item.ItemStack;
 
-import java.awt.*;
 import java.io.IOException;
 
 public class YuLuowoGui extends GuiScreen {
@@ -16,7 +17,13 @@ public class YuLuowoGui extends GuiScreen {
     public void initGui() {
         super.initGui();
         // drawRect mean background
-        this.buttonList.add(btn_Close = new GuiButton(0, 300, 300, "Close"));
+        ScaledResolution resolution = new ScaledResolution(Minecraft.getMinecraft());
+        this.width = resolution.getScaledWidth();
+        this.height = resolution.getScaledHeight();
+        int x = (this.width) / 2;
+        int y = (this.height) / 2;
+        this.buttonList.add(btn_Close = new GuiButton(0, 5, 5, 80, 20, "Close"));
+        //this.buttonList.add(btn_Close = new GuiButton(0, x - 200 / 2, y - 20 / 2, "Close"));
     }
 
     @Override
@@ -29,6 +36,9 @@ public class YuLuowoGui extends GuiScreen {
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawDefaultBackground();
+
+
+
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
