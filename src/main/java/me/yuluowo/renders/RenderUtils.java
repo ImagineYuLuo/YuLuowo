942fd5f66e582a48bbd976aefccd054263c15585
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-23 16:45:30 +0800

    Update RenderUtils.java

--- src/main/java/me/yuluowo/renders/RenderUtils.java
+++ src/main/java/me/yuluowo/renders/RenderUtils.java
@@ -3,6 +3,10 @@ package me.yuluowo.renders;
 import me.yuluowo.commands.WayPoints;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
@@ -20,7 +24,6 @@ public class RenderUtils {
                 fontRenderer.drawString("Waypoint set at : " + x + " " + y + " " + z, 5, 5, 0);
             }else {
                 fontRenderer.drawString(null, 5, 5, 0);
-
             }
         }
     }
