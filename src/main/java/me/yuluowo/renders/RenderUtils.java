2d1d8a45c7077145f19e88190b6e0a2240639a43
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2023-11-28 17:37:32 +0800

    Update RenderUtils.java

--- src/main/java/me/yuluowo/renders/RenderUtils.java
+++ src/main/java/me/yuluowo/renders/RenderUtils.java
@@ -3,21 +3,18 @@ package me.yuluowo.renders;
 import me.yuluowo.commands.WayPoints;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldRenderer;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class RenderUtils {
+
+    private static final Minecraft mc = Minecraft.getMinecraft();
 
     @SubscribeEvent
     public void onRenderTick(TickEvent.RenderTickEvent event){
         int x = WayPoints.playerX;
         int y = WayPoints.playerY;
         int z = WayPoints.playerZ;
-        Minecraft mc = Minecraft.getMinecraft();
         FontRenderer fontRenderer = mc.fontRendererObj;
         if(mc.theWorld != null){
             if(WayPoints.showWayPoint()){
@@ -26,6 +23,12 @@ public class RenderUtils {
                 fontRenderer.drawString(null, 5, 5, 0);
             }
         }
+    }
+
+    public static void drawFontCenter(String text, int x, int y, int color){
+        FontRenderer fontRenderer = mc.fontRendererObj;
+        int halfWidth = fontRenderer.getStringWidth(text) / 2;
+        fontRenderer.drawString(text, x - halfWidth, y, color);
     }
 
 }
