18feff8dcbd8a9258a7c315e8eb52aa92f3409b4
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2024-01-18 10:37:01 +0800

    Update YuLuowoGui.java

--- src/main/java/me/yuluowo/gui/YuLuowoGui.java
+++ src/main/java/me/yuluowo/gui/YuLuowoGui.java
@@ -1,9 +1,9 @@
 package me.yuluowo.gui;
 
+import me.yuluowo.gui.elements.GButton;
 import me.yuluowo.renders.RenderUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.*;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import org.lwjgl.opengl.GL11;
 
@@ -13,11 +13,11 @@ import java.io.IOException;
 
 public class YuLuowoGui extends GuiScreen {
 
-    private GuiButton btn_OnOff;
-    private GuiButton btn_Close;
+    private GuiButton buttonOnOff;
+    private GuiButton buttonClose;
 
-    private static final int btn_width = 100;
-    private static final int btn_height = 20;
+    private static final int buttonWidth = 100;
+    private static final int buttonHeight = 20;
 
     private final int scale = 70;
 
@@ -28,25 +28,26 @@ public class YuLuowoGui extends GuiScreen {
         this.height = resolution.getScaledHeight();
         int x = (this.width) / 2;
         int y = (this.height) / 2;
-        this.buttonList.add(btn_OnOff = new GuiButton(0, x - btn_width / 2, y - btn_height / 2 + 50, btn_width, btn_height, "On"));
-        this.buttonList.add(btn_Close = new GuiButton(1, x - btn_width / 2, y - btn_height / 2, btn_width, btn_height, "Close"));
+        this.buttonList.add(buttonOnOff = new GuiButton(0, x - buttonWidth / 2, y - buttonHeight / 2 + 50, buttonWidth, buttonHeight, "On"));
+        this.buttonList.add(buttonClose = new GuiButton(1, x - buttonWidth / 2, y - buttonHeight / 2, buttonWidth, buttonHeight, "Close"));
         super.initGui();
     }
 
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
-        if (button == btn_Close) {
+        if (button == buttonClose) {
             mc.thePlayer.closeScreen();
-        }else if (button == btn_OnOff) {
-            btn_OnOff.displayString = ((btn_OnOff.displayString.equals("On")) ? "Off" : "On");
+
+        }else if (button == buttonOnOff) {
+            buttonOnOff.displayString = ((buttonOnOff.displayString.equals("On")) ? "Off" : "On");
         }
     }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         Gui.drawRect(scale, scale, width - scale, height - scale, new Color(0, 0, 0, 150).getRGB());
+        RenderUtils.drawRect();
         super.drawScreen(mouseX, mouseY, partialTicks);
-
         RenderUtils.drawFontCenter("YuLuoAddons", width / 2, scale, 0xFFFFFF);
     }
 
