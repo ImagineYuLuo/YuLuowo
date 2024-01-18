30bf2d390aba1949f101518a97316d86d66d0df1
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2024-01-18 10:37:19 +0800

    Update ScoreboardUtils.java

--- src/main/java/me/yuluowo/utils/ScoreboardUtils.java
+++ src/main/java/me/yuluowo/utils/ScoreboardUtils.java
@@ -1,7 +1,24 @@
 package me.yuluowo.utils;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.scoreboard.ScoreObjective;
+import net.minecraft.scoreboard.Scoreboard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ScoreboardUtils {
+    private static final List<Location> locationNameList = new ArrayList<>(Arrays.asList(Location.ISLAND, Location.GUEST_ISLAND));
+    private static final Minecraft mc = Minecraft.getMinecraft();
 
-
+    public static boolean getCurrentLocation(String locationName){
+        if(Utils.isOnHypixel() && Utils.isOnSkyblock()){
+            Scoreboard scoreboard = mc.theWorld.getScoreboard();
+            ScoreObjective scoreObjective = scoreboard.getObjectiveInDisplaySlot(1);
+            return scoreObjective.getDisplayName().equals(locationName);
+        }
+        return false;
+    }
 
 }
