b0392adc3cb1293394a1613613159f9576a0656b
YuLuo <129162114+ImagineYuLuo@users.noreply.github.com>
2024-06-20 16:29:01 +0800

    Revert "Update YuLuo.java"
    This reverts commit dadfb95b5ab00bc82dd02556ef741eee690edb7f.

--- src/main/java/me/yuluowo/YuLuo.java
+++ src/main/java/me/yuluowo/YuLuo.java
@@ -1,6 +1,18 @@
 package me.yuluowo;
 
+import me.yuluowo.commands.Config;
+import me.yuluowo.commands.PartyTransfer;
+import me.yuluowo.commands.DisplayItemName;
+import me.yuluowo.commands.WayPoints;
+import me.yuluowo.commands.macros.Mining;
+import me.yuluowo.commands.warp.JoinDungeons;
+import me.yuluowo.commands.warp.WarpCommand;
+import me.yuluowo.events.ChatMessage;
+import me.yuluowo.module.macros.MiningMacro;
+import me.yuluowo.renders.RenderUtils;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
@@ -16,6 +28,18 @@ public class YuLuo {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new ChatMessage());
+        MinecraftForge.EVENT_BUS.register(new RenderUtils());
+        MinecraftForge.EVENT_BUS.register(new Config());
+        MinecraftForge.EVENT_BUS.register(new MiningMacro());
 
+        ClientCommandHandler.instance.registerCommand(new Config());
+        ClientCommandHandler.instance.registerCommand(new JoinDungeons());
+        ClientCommandHandler.instance.registerCommand(new WarpCommand());
+        ClientCommandHandler.instance.registerCommand(new PartyTransfer());
+        ClientCommandHandler.instance.registerCommand(new DisplayItemName());
+        ClientCommandHandler.instance.registerCommand(new Mining());
+
+        ClientCommandHandler.instance.registerCommand(new WayPoints.getWayPoints());
     }
 }
